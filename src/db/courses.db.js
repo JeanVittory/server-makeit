b69465a6238bdf51f527bc6de25e22edacf4f58f
JeanVittory <vittory.dev@gmail.com>
2023-02-14 15:52:28 -0500

    update: Adding course information to database folder

---
+++ src/db/courses.db.js
@@ -0,0 +1,10 @@
+const data = {
+	message: 'Curso encontrado satisfactoriamente',
+	top: {
+		version_top: 27,
+		students: 10,
+		names: ['Oscar', 'Nicolas', 'Jean', 'Diego', 'Camilo', 'Michael', 'Andres', 'Sebastian'],
+	},
+};
+
+module.exports = data;
--- src/handlers/getHandler.js
+++ src/handlers/getHandler.js
@@ -1,12 +1,6 @@
+const data = require('../db/courses.db.js');
+
 const getHandler = (req, res) => {
-	const data = {
-		message: 'Curso encontrado satisfactoriamente',
-		top: {
-			version_top: 27,
-			students: 10,
-			names: ['Oscar', 'Nicolas', 'Jean', 'Diego', 'Camilo', 'Michael', 'Andres', 'Sebastian'],
-		},
-	};
 	res.status(200).json(data);
 };
 
